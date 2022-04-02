cc9e72aba11c57c9116a25942d99e1e5e2fe03e5
actis_t@mail.ru <actis_t@mail.ru>
2022-04-02 13:10:32 -0700

    technology stack was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ResponcibilitiesListComponent } from './responcibilities-list/responcibilities-list.component';
-import { ResponsibilityDetailComponent } from './responcibilities-list/responsibility-detail/responsibility-detail.component';
 import { Responsibility } from './responcibilities-list/responsibility.model';
 
 @Component({
@@ -12,10 +10,12 @@ export class AppComponent {
   title = 'Grigoryeva Tatiana';
   responsibilities: Responsibility[] = [new Responsibility("GDC",
     ["As application support - implement changes and improvements, resolve problems and incidents, raised by customers",
-      "Create functional design, which describes all the changes needed to be implemented"]),
+      "Create functional design, which describes all the changes needed to be implemented"],
+    ["Entity Framework", "C#", "T-SQL"]  ),
   new Responsibility("Arcadia",
     ["Create authentication using access and refresh tokens",
-      "Create new endpoint for creating base entity that app is working with"])
+      "Create new endpoint for creating base entity that app is working with"],
+    ["ADO.NET", "C#", "API", ".Net Core", "Unit tests"]  )
   ];
   get count(): number {
     return this.responsibilities.length
--- src/app/responcibilities-list/responsibility.model.ts
+++ src/app/responcibilities-list/responsibility.model.ts
@@ -1,10 +1,12 @@
 
 export class Responsibility {
   public responsibility: string[];
-  public companyName: string
+  public companyName: string;
+  public stack: string[];
 
-  constructor(companyName:string, responsibility: string[]) {
+  constructor(companyName: string, responsibility: string[], stack: string[]) {
     this.responsibility = responsibility;
-    this.companyName = companyName
+    this.companyName = companyName;
+    this.stack = stack;
   }
 }
